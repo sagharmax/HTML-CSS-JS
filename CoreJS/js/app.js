339b2b8f8e733087716a291a86f537549f1587a1
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-13 19:18:14 +0330

    Variables

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,2 +1,22 @@
-console.log('Hello World from another file');
-console.log('This is Saeid Ghasemi');
+// var testString = 'This is a test';
+let testString = 'This is a test';
+const testNumber = 45.87;
+const testBoolean = true;
+// const testconst;
+testString = 'This is a better test';
+console.log(testString);
+
+/*
+i = 10;
+i = 5;
+console.log(i);
+var i = 2;
+*/
+
+/*
+j = 10;
+j = 5;
+console.log(j);
+let j = 2;
+*/
+
