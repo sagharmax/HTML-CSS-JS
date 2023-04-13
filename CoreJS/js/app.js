c885af9b3770ec8728b9d095273443e505460029
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-13 22:33:12 +0330

    Conditional

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,22 +1,36 @@
-const people = ['Saeid', 'Mohaddeseh', 'Sepanta', 'Shakila'];
-people.push('Tamay');
-const lastPerson = people.pop()
-console.log(people);
-console.log(people.indexOf('Shakila'));
-console.log(people.indexOf('Tamay'));
-console.log(Array.isArray(people));
-console.log(Array.isArray(lastPerson));
-console.log(lastPerson.startsWith('Ta'));
-people.push('Bahman');
-people.push('Ferina');
-people.push('Naeimeh');
-const coolPeople = people.filter(function(person) {
-    return person.startsWith('S') === true;
-})
-console.log(coolPeople);
-console.log(people);
-console.log(lastPerson.substring(0, 3));
-const firstLetters = people.map(function(person) {
-    return person.substring(0, 1);
-});
-console.log(firstLetters);
+const firstName = 'Saeid';
+const middleName = 'SagharMax';
+const lastName = 'Ghasemi';
+// if (firstName.toLowerCase() === 'saeid') {
+if (firstName === 'Saeid' && lastName == 'Ghasemi') {
+    console.log('Hello programmer');
+} else if (firstName === 'Saeid') {
+    console.log('Hello other Saeid');
+} else if (lastName === "Ghasemi") {
+    console.log('Cool last name');
+} else {
+    console.log('Hello anyway');
+}
+
+if ((firstName === "Saeid" || lastName === "Ghasemi") && middleName === "SagharMax") {
+    console.log('That\'s pretty cool name');
+}
+
+const x = '1';
+const y = 1;
+if (x === y) { // === is type and value comparison
+    console.log('The two values are equal');
+}
+
+const day = 'Tuesday';
+switch (day) {
+    case 'Monday':
+        console.log('Welcome to the first of the week.');
+        break;
+    case 'Tuesday':
+        console.log('I hope your week is going well.');
+        break;
+    default:
+        console.log('I do not know what day of the week that is.');
+        break;
+}
