03dbf157a4fd67e8b08dcda674ddec38f4260ce4
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-13 23:46:43 +0330

    Loops

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,36 +1,22 @@
-const firstName = 'Saeid';
-const middleName = 'SagharMax';
-const lastName = 'Ghasemi';
-// if (firstName.toLowerCase() === 'saeid') {
-if (firstName === 'Saeid' && lastName == 'Ghasemi') {
-    console.log('Hello programmer');
-} else if (firstName === 'Saeid') {
-    console.log('Hello other Saeid');
-} else if (lastName === "Ghasemi") {
-    console.log('Cool last name');
-} else {
-    console.log('Hello anyway');
+const people = ['Saeid', 'Mohaddeseh', 'Sepanta', 'Shakila', 'Tamay'];
+
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i]);
+};
+
+
+for (const person of people) {
+    console.log(person);
 }
 
-if ((firstName === "Saeid" || lastName === "Ghasemi") && middleName === "SagharMax") {
-    console.log('That\'s pretty cool name');
-}
 
-const x = '1';
-const y = 1;
-if (x === y) { // === is type and value comparison
-    console.log('The two values are equal');
-}
+people.forEach(function(person) {
+    console.log(person);
+});
 
-const day = 'Tuesday';
-switch (day) {
-    case 'Monday':
-        console.log('Welcome to the first of the week.');
-        break;
-    case 'Tuesday':
-        console.log('I hope your week is going well.');
-        break;
-    default:
-        console.log('I do not know what day of the week that is.');
-        break;
+
+while (people.length > 0) {
+    console.log(people.pop());
 }
+console.log(people);
