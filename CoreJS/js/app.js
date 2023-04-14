db8a2d96a9e080c0790c42c722fbedc319c0de3b
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-14 13:55:00 +0330

    Scope

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,41 +1,28 @@
-// IIFE : Immediately Invoking Function Expression
-const myName = 'Saeid Ghasemi'
-
-function greetUser() {
-    console.log('Welcome to our app');
+var a = 4;
+function testing() {
+    var a = 5;
+    console.log(`Inside testing(): ${a}`);
 }
-
-(function(app, fullName) {
-    fullName = fullName; // property on anonymous function
-    app.greetUser = function() {
-        console.log(`Hello ${fullName}`);
-    }
-    app.Person = class {
-        constructor(firstName, lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-    }
-})(window.app = window.app || {}, myName);
-console.log(window.app);
-greetUser()
-app.greetUser();
-const p = new app.Person('Saeid', 'Ghasemi');
-const q = new app.Person('Mohaddeseh', 'AmiriNezhad');
-console.log(q.lastName);
+testing();
+console.log(`global: ${a}`);
 
 
-(function(app) {
-    app.sayGoodbye = function() {
-        console.log('Goodbye my favorite user');
-    }
-})(window.app = window.app || {})
-app.sayGoodbye();
+var b = 4;
+if (true) {
+    var b = 5
+}
+console.log(b);
 
 
-(function(app) {
-    app.greetUser = function() {
-        console.log('Welcome to our other app');
-    }
-})(window.otherApp = window.otherApp || {})
-otherApp.greetUser();
+let c = 4;
+if (true) {
+    let c = 5
+}
+console.log(c);
+
+
+const d = 4;
+if (true) {
+    const d = 5
+}
+console.log(d);
