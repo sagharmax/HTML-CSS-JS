13684e1f8dc098ece30900ee880839bc5f9058de
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-14 15:25:26 +0330

    Setting Up our Test Code

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,15 +1,13 @@
-/*
-
-BEST PRACTICES
-==============
-
-1. Add 'use strict'; ti the top of every file and IIFE.
-2. Do not use var - use let or const instead. Prefer const.
-3. Naming - use camelCase for variables, functions, etc. and PascalCase for classes.
-4. Use IIFE's whenever it makes sense - it almost always makes sense.
-5. Just because you can, doesn't mean you should.
-6. Use a peparate file for your JavaScript.
-7. Use semi-colons.
-8. Don't Assume.
-
-*/
+(function(app) {
+    'use strict';
+    app.divide = function(x, y) {
+        return x / y;
+    };
+    app.complicatedFormula = function(x) {
+        let result = x * 3 + 2;
+        x += 4;
+        result += this.divide(result, 2)
+        return result;
+    }
+})(window.app = window.app || {});
+console.log(app.complicatedFormula(23));
