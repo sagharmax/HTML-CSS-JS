12b882bf62dfe5add5092e4253591240a40747c6
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-14 13:06:37 +0330

    Classes

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,55 +1,30 @@
-const person = {
-    firstName: 'Saeid',
-    lastName: 'Ghasemi',
-    age: 39,
-    isAlive: true,
-    address: {
-        city: 'Urmia',
-        country: 'Iran'
-    },
-    fullName: function() {
+// class = blueprint, class instance = house
+class Person {
+    #socialSecurityNumber = '';
+
+    constructor (firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    get ssn() {
+        return `***-**-${this.#socialSecurityNumber.substring(this.#socialSecurityNumber.length - 4)}`;
+    }
+    set ssn(social) {
+        this.#socialSecurityNumber = social;
+    }
+    // getFullName = () => `${this.firstName} ${this.lastName}`;
+    getFullName() {
         return `${this.firstName} ${this.lastName}`;
     }
-};
-console.log(person);
-console.log(person.firstName);
-console.log(person.lastName);
-console.log(person.age);
-console.log(person.isAlive);
-console.log(person.address.city);
-console.log(person.address.country);
-person.address.country = 'Turkey';
-console.log(person.address.country);
-console.log(person.fullName());
-
-
-function greetUser(p) {
-    console.log(`Hello ${p.fullName()}`);
 }
-greetUser(person)
+const person1 = new Person('Saeid', 'Ghasemi');
+console.log(person1.firstName);
+console.log(person1.lastName);
+const person2 = new Person('Mohaddeseh', 'AmiriNezhad');
+console.log(person2.firstName);
+console.log(person2.lastName);
 
 
-// Deconstructor
-const { firstName:fn, age, address: {city} } = person;
-// const fn = person.firstName;
-// const age = person.age;
-// const city = person.address.city;
-// console.log(firstName);
-console.log(age);
-console.log(city);
-console.log(fn);
-
-
-for (const prop in person) {
-    console.log(prop);
-    console.log(`${prop}: ${person[prop]}`);
-    if (person.hasOwnProperty(prop)) {
-        console.log(`${prop}: ${person[prop]}`);
-    }
-}
-delete person.fullName;
-// console.log(person.fullName());
-// console.log(JSON.stringify(person));
-const recivedInfo = JSON.stringify(person);
-const parsedInfo = JSON.parse(recivedInfo);
-console.log(parsedInfo.firstName);
+person1.ssn = '123-45-6789';
+console.log(person1.ssn);
+console.log(person1.getFullName());
