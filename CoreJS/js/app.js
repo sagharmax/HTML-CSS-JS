00b815882a566e00bb22d06e1f11aa3a7d59a56c
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-14 15:56:48 +0330

    Console & Breakpoints in browser

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,13 +1,18 @@
 (function(app) {
     'use strict';
     app.divide = function(x, y) {
+        console.log(x/y);
         return x / y;
     };
     app.complicatedFormula = function(x) {
         let result = x * 3 + 2;
-        x += 4;
-        result += this.divide(result, 2)
+        result += 4;
+        console.log(result);
+        result = this.divide(result, 2)
+        console.log(result);
         return result;
     }
 })(window.app = window.app || {});
 console.log(app.complicatedFormula(23));
+// console.warn('This is a warning');
+// console.error('This is an error');
