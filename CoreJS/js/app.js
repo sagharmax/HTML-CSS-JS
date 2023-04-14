48a5a17bd5f9c8b2dcae2cdaad79ca3beaa46e9e
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-14 14:29:48 +0330

    Best Practices

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,27 +1,15 @@
-'use strict';
+/*
 
-// let myTest = 2;
-// mytest = 3;
-// console.log(myTest);
+BEST PRACTICES
+==============
 
+1. Add 'use strict'; ti the top of every file and IIFE.
+2. Do not use var - use let or const instead. Prefer const.
+3. Naming - use camelCase for variables, functions, etc. and PascalCase for classes.
+4. Use IIFE's whenever it makes sense - it almost always makes sense.
+5. Just because you can, doesn't mean you should.
+6. Use a peparate file for your JavaScript.
+7. Use semi-colons.
+8. Don't Assume.
 
-// 'tset'.myInfo = 'Hello';
-// console.log('test'.myInfo);
-
-
-// function add(x, x) {
-//     return x + x;
-// }
-// console.log(add(5, 4));
-
-
-// function test() {
-//     'use strict';
-//     test1 = 2
-// }
-// test()
-
-
-// (function(app) {
-//     'use strict';
-// })();
+*/
