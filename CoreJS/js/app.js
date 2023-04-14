82ef6c05f68277547bb4681cad1550163653fb37
Saeid Ghasemi <sagharmax@outlook.com>
2023-04-14 10:42:06 +0330

    Functions

--- CoreJS/js/app.js
+++ CoreJS/js/app.js
@@ -1,22 +1,35 @@
-const people = ['Saeid', 'Mohaddeseh', 'Sepanta', 'Shakila', 'Tamay'];
-
-
-for (let i = 0; i < people.length; i++) {
-    console.log(people[i]);
-};
-
-
-for (const person of people) {
-    console.log(person);
+function add(x = 2, y = 6) {
+    return x + y;
 }
+let result = add(2, 3);
+console.log(result);
+result = add(4, 5);
+console.log(result);
+console.log(add(2, 3));
+const moreResult = add(3, 8) + add (2, 11);
+console.log(moreResult);
+console.log(add(5));
 
 
-people.forEach(function(person) {
-    console.log(person);
-});
-
-
-while (people.length > 0) {
-    console.log(people.pop());
+function greetUser() {
+    console.log('Hello User');
+    console.log('Welcome to our website.');
+    console.log('We hope you enjoy it.');
+    console.log(`The value of 5 + 3 = ${add(5, 3)}`);
 }
-console.log(people);
+greetUser()
+
+
+/*
+const subtract = (x, y) => {
+    return x - y;
+}
+*/
+const subtract = (x, y) => x - y;
+console.log(subtract(9, 3));
+const people = ['Saeid', 'Mohaddeseh', 'Sepanta'];
+//const filtered = people.filter(p => p.substring(0, 1) === 'S');
+const filtered = people.filter(function(p) {
+    return p.substring(0, 1) === 'S';
+})
+console.log(filtered);
